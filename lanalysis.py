b51e5614ebb508bb20939d69c8060f8b29bf7bc3
Lokesh Patil <42104811+lokesh-ghoul@users.noreply.github.com>
2021-02-20 18:24:11 +0530

    linux-log

---
+++ lanalysis.py
@@ -0,0 +1,13 @@
+f = open("/var/log/dpkg.log","r")
+#count = len(open("/var/log/xrdp.log","r").readlines(  ))
+#print(count)
+count = 0
+fdict = {}
+for x in f:
+	#print(x)
+	xsplit = x.split(' ',2)
+	#print(xsplit[0], xsplit[1], xsplit[2])
+	#print(xsplit[2].strip(' ]'))
+	fdict[count] = {'Date:Time':xsplit[0],'Type':xsplit[1],'Detail':xsplit[2].strip(' ]')}
+	count = count+1	
+print(fdict[0])
